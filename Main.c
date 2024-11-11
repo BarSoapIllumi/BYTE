1504452dc90d203e4c0799ddf5afebb8bbd9d5e5
Angela <angela.zhou12345@gmail.com>
2024-11-11 14:17:11 -0500

    completeed functions

--- Main.c
+++ Main.c
@@ -1,15 +1,15 @@
 // Motors
 const string MF = "motorA"; // Front motor
-const string MB = "motorD"; // Back motor
-const string MW = "motorB"; // Turning wheel motor
+const string MBL = "motorD"; // Back left motor
+const string MBR = "motorB"; // Back right motor
 const string MT = "motorD"; // Tray motor
 
 // Sensors
-const string T = "S1" // Touch sensor
-const string US = "S2" // Ultrasonic sensor
-const string C = "S3" // Color sensor
-const string G = "S4" // Gyro sensor
-const string IR = "S#" // IR Sensor
+const string T = "S1"; // Touch sensor
+const string US = "S2"; // Ultrasonic sensor
+const string C = "S3"; // Color sensor
+const string G = "S4"; // Gyro sensor
+const string IR = "S####"; // IR Sensor
 
 // Configures all necessary sensors
 void configureSensors(){
@@ -43,8 +43,74 @@ void configureSensors(){
 	wait1Msec(50);
 }
 
+/*
+Travels to an indicated IR beacon
+@param beacon indicated beacon channel
+*/
+void moveToBeacon(int beacon){
+
+	while(getIRBeaconChannelDirection(IR, beacon) < -5 || getIRBeaconChannelDirection(IR, beacon) > 5){ // Detects heading
+		if (getIRBeaconChannelDirection(IR, beacon) < 0) { // If IR beacon is to the left, turn right
+			motor[MBL] = -50;
+			motor[MBR] = 50;
+			motor[MF] = 50;
+		} else { // If IR beacon is to the right, turn left
+			motor[MBL] = 50;
+			motor[MBR] = -50;
+			motor[MF] = 50;
+		}
+	}
+	motor[MBL] = motor[MBR] = motor[MF] = 0;
+
+	motor[MBL] = motor[MBR] = motor[MF] = 50; // Move forwards
+	while(getIRBeaconChannelStrength(IR, beacon) > 10) { // Detects distance from beacon
+		// OBSTACLE AVOID FUNCTION
+	}
+	motor[MBL] = motor[MBR] = motor[MF] = 0;
+
+}
+
+/*
+Brings robot to specified food pile, intakes food, then returns home
+@param beacon indicated food pile beacon channel
+@return time timeed taken to complete functions
+*/
+int getFood(int beacon){
+
+	clearTimer(T1); // Reset timer
+
+	moveToBeacon(beacon);
+
+	Suck_Spit(1); // Intakes food
+
+	moveToBeacon(0); //REPLACE WITH HOME
+
+	Suck_Spit(0); // Spits out food
+
+	return time1[T1];
+
+}
+
+// BYTE startup procedure
+void start(){
+
+	while(!getButtonPress(buttonEnter)){} // Wait for button enter to be pressed
+	roaming();
+	//SOUND SENSOR STUFF
+
+	moveToBeacon(0); // REPLACE WITH HOME
+
+}
+
 // Main Function
 task main()
 {
-	configureSensors();
+	configureSensors(); // Configure all sensors
+
+	start(); // Begin start procedure
+
+	while(true){ // All processes here
+
+	}
+
 }
