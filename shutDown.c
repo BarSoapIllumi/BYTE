b3528fba2c75f48c7770263cfb322be110b8f60c
SAT-oO <z869liu@uwaterloo.ca>
2024-11-20 19:59:46 -0500

    shutdown procedure: press enter button twice and deject for 10 secs

---
+++ shutDown.c
@@ -0,0 +1,38 @@
+
+const string MT = "motorA"; // Tray motor
+
+
+void Suck_Spit (bool indicator, int time){
+    if (indicator == 1){
+        motor[MT] = 25;
+    }
+    else{
+        motor[MT] = -25;
+    }
+	wait1Msec(time);
+    motor[MT] = 0;
+}
+
+
+void shutDown(){
+	while(!getButtonPressed(ButtonEnter)){}
+	while(getButtonPressed(ButtonEnter)){}
+	wait1Msec(50);
+
+	while(!getButtonPressed(ButtonEnter)){}
+	while(getButtonPressed(ButtonEnter)){}
+
+	Suck_Spit(0, 10000);
+
+}
+
+task main()
+{
+	configureSensors();
+	shutDown();
+
+
+
+
+
+}
