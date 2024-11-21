8c540a1cb07db02176562e7fdce4909745580410
SAT-oO <z869liu@uwaterloo.ca>
2024-11-20 20:19:20 -0500

    updated version of roaming

---
+++ roaming.c
@@ -0,0 +1,33 @@
+//roaming motion cycles: left right turn
+
+void roaming(){ 
+	wait1Msec(1000); //left
+	motor[MBL] = 50;
+	motor[MBR] = 0;
+
+	wait1Msec(100);//right
+	motor[MBL] = 0;
+	motor[MBR] = 50;
+
+	
+	resetGyro; //turn
+	motor[MBL] = 50;
+	while(abs(getGyroDegrees[gyroPort]) < 360){}
+	motor[MBL] = 0;
+
+
+
+}
+
+
+
+
+task main()
+{
+	
+	roaming();
+	
+
+
+
+}
